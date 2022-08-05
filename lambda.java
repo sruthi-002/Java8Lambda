f5703c4bae1e9f56f519d29c38e5c3607bcd5a64
sruthi-002 <89520373+sruthi-002@users.noreply.github.com>
2022-08-05 15:50:49 +0530

    Add files via upload

---
+++ lambda.java
@@ -0,0 +1,15 @@
+﻿package com.company;
+import java.util.Scanner;
+interface adding{
+    int add(int a , int b);
+}
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        adding add1 = (a,b) -> (a+b) ;
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println(add1.add(a,b));
+    }
+}
